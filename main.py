493a8630f56745bf39953b3927ec28cb8b0403f4
Deniz <denizkozkan@hotmail.com>
2023-10-10 22:18:09 +0300

    Ad random user

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import random
+
+
 def print_board(board):
     for row in board:
         print(" | ".join(row))
-        if row != board[-1]:
-            print("-" * 9)
+        # if row != board[-1]:
+        #     print("-" * 9)
 
 
 def check_win(board, player):
@@ -24,6 +27,22 @@ def is_full(board):
     return all(cell != " " for row in board for cell in row)
 
 
+def player_move(board, player):
+    while True:
+        print_board(board)
+        if player == "X":
+            row, col = map(int, input(f"Player {player}, enter your row and column (e.g., 1 2): ").split())
+        else:
+            row, col = random.randint(1, 3), random.randint(1, 3)
+
+        if row < 1 or row > 3 or col < 1 or col > 3 or board[row - 1][col - 1] != " ":
+            print("Invalid move. Try again.")
+            continue
+
+        board[row - 1][col - 1] = player
+        break
+
+
 def main():
     board = [[" " for _ in range(3)] for _ in range(3)]
     current_player = "X"
@@ -31,25 +50,27 @@ def main():
     print("Welcome to Tic Tac Toe!")
 
     while True:
-        print_board(board)
-        row, col = map(int, input(f"Player {current_player}, enter your row and column (e.g., 1 2): ").split())
-
-        if row < 1 or row > 3 or col < 1 or col > 3 or board[row - 1][col - 1] != " ":
-            print("Invalid move. Try again.")
-            continue
-
-        board[row - 1][col - 1] = current_player
-
-        if check_win(board, current_player):
+        player_move(board, "X")
+        if check_win(board, "X"):
             print_board(board)
-            print(f"Player {current_player} wins! Congratulations!")
+            print("Player X wins! Congratulations!")
             break
+
         elif is_full(board):
             print_board(board)
             print("It's a draw!")
             break
 
-        current_player = "O" if current_player == "X" else "X"
+        player_move(board, "O")
+        if check_win(board, "O"):
+            print_board(board)
+            print("Player O wins! Congratulations!")
+            break
+
+        elif is_full(board):
+            print_board(board)
+            print("It's a draw!")
+            break
 
 
 if __name__ == "__main__":
